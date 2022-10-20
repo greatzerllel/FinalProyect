2f14723f32e2b9f377504d35dfaf4470b871cbe2
ZERLLEL <greatzerllel2@gmail.com>
2022-10-20 17:39:17 +0000

    estructura-admin3

--- src/front/js/component/sideBar.js
+++ src/front/js/component/sideBar.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const SideBar = () => {
   return (
-    <div className='col-md-2 bg-white'>sideBar</div>
-  )
-}
-
+    <>
+      {/* <div className='col-md-2 bg-white'>sideBar</div> */}
+      <div className="sidebar">
+        <ul>
+          <li>
+            <Link to="">Productos publicados</Link>
+          </li>
+          <li>
+            <Link to="">Pedidos</Link>
+          </li>
+          <li>
+            <Link to="">AHistorial</Link>
+          </li>
+          <li>
+            <Link to="">Administrar cuentas</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
